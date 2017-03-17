d767830968a54e02a2e6e3168dd86af94022f319
Quentin Honore <qhonore@emi.u-bordeaux1.fr>
2017-03-17 15:21:10 +0100

    Changement convolution pour faire marcher à Sobel, en cours

--- app/src/main/java/com/example/emmaveillat/finalproject/ConvolutionMenu.java
+++ app/src/main/java/com/example/emmaveillat/finalproject/ConvolutionMenu.java
@@ -94,26 +94,21 @@ public class ConvolutionMenu extends AppCompatActivity {
         int[][] mask ={{1,2,3,2,1},{2,6,8,6,2},{3,8,10,8,3},{2,6,8,6,2},{1,2,3,2,1}};
         convolution(bmp, mask, 98);
     }
-
+    //A TRAVAILLER
     public void sobel(Bitmap bmp){
         int width = bmp.getWidth();
         int height = bmp.getHeight();
+        int[] Gx = new int[height*width];
+        int[] Gy = new int[height*width];
         //applicates convolution with hx et hy matrix
         int[][] hx = {{-1,0,1},{-2,0,2},{-1,0,1}};
-        Bitmap Gx = bmp;
-        convolution(Gx,hx,1);
+        convolution(bmp,hx,1);
         int[][] hy = {{-1,-2,-1},{0,0,0},{1,2,1}};
-        Bitmap Gy = bmp;
-        convolution(Gy,hy,1);
-        int[] pixX = new int[height*width];
-        int[] pixY = new int[height*width];
-        Gx.getPixels(pixX, 0, width, 0, 0, width, height);
-        Gy.getPixels(pixY, 0, width, 0, 0, width, height);
+        convolution(bmp,hy,1);
         //On every pixels, calculates sqrt(x^2 + y^2) from gx and gy
         for (int i=0; i<height*width; i++){
-            pixX[i] = (int) Math.sqrt(Math.pow(pixX[i],2)+Math.pow(pixY[i],2));
+            Gx[i] = (int) Math.sqrt(Math.pow(Gx[i],2)+Math.pow(Gy[i],2));
         }
-        bmp.setPixels(pixX, 0, width, 0, 0, width, height);
     }
 
     public void laplacien(Bitmap bmp){
@@ -122,7 +117,7 @@ public class ConvolutionMenu extends AppCompatActivity {
         convolution(bmp, mask, 1);
     }
 
-    public void convolution(Bitmap bmp, int[][] mask, int factor) {
+    public int[] convolution(Bitmap bmp, int[][] mask, int factor) {
         int n = mask.length / 2;
         if (n!=0) {
             int width = bmp.getWidth();
@@ -160,7 +155,7 @@ public class ConvolutionMenu extends AppCompatActivity {
                     sumR = 0;
                     sumG = 0;
                     sumB = 0;
-                    A = Color.alpha(pixels[y*width+x]);
+                    A = Color.alpha(pixels[y * width + x]);
                     for (int j = -n; j <= n; j++) {
                         for (int i = -n; i <= n; i++) {
                             float coef_mask = mask[j + n][i + n];
@@ -171,6 +166,10 @@ public class ConvolutionMenu extends AppCompatActivity {
                             sumB += coef_mask * Color.blue(pixel);
                         }
                     }
+                }
+            }
+            return pixelsConv;
+        }
 
                     //If value is too low, sets to 0, if too up, sets to 255
 
