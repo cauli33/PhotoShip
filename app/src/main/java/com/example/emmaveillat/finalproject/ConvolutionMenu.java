4e3bb4bae5d185cc8bf76ddd2677930aa6f37cc9
Cauli33 <caulihonore@gmail.com>
2017-03-25 16:34:47 +0100

    Cartoon convenable, à revoir peut être

--- app/src/main/java/com/example/emmaveillat/finalproject/ConvolutionMenu.java
+++ app/src/main/java/com/example/emmaveillat/finalproject/ConvolutionMenu.java
@@ -1,15 +1,18 @@
 package com.example.emmaveillat.finalproject;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.util.Arrays;
 
@@ -32,7 +35,7 @@ public class ConvolutionMenu extends AppCompatActivity {
     /**
      * Text choosen by the user
      */
-    EditText facteurLongueur;
+    EditText lengthText;
 
     /**
      * Buttons to apply functions, to save or reset a bitmap
@@ -50,8 +53,8 @@ public class ConvolutionMenu extends AppCompatActivity {
         setContentView(R.layout.activity_convolution_menu);
 
         // Creates textzone to choose matrix length for moyenne
-        facteurLongueur = (EditText) findViewById(R.id.newLength);
-        facteurLongueur.setHint("Notez ici la taille de la matrice");
+        lengthText = (EditText) findViewById(R.id.newLength);
+        lengthText.setHint("Notez ici la taille de la matrice (impair)");
 
         // Gets picture Bitmap chosen in the gallery
         pictureToUse = PhotoLoading.scaleImage();
@@ -83,6 +86,40 @@ public class ConvolutionMenu extends AppCompatActivity {
         cartoon.setOnClickListener(blistener);
     }
 
+
+    public Bitmap reduce(Bitmap bmp){
+        int oldWidth = bmp.getWidth();
+        int oldHeight = bmp.getHeight();
+        if (oldWidth*oldHeight> 1000000) {
+            int newWidth, newHeight;
+            float fact;
+            if (oldWidth > oldHeight) {
+                newWidth = 800;
+                newHeight = newWidth * oldHeight / oldWidth;
+                fact = oldWidth / 800F;
+            } else {
+                newHeight = 800;
+                newWidth = newHeight * oldWidth / oldHeight;
+                fact = oldHeight / 800F;
+            }
+            int[] mapSrc = new int[oldWidth * oldHeight];
+            bmp.getPixels(mapSrc, 0, oldWidth, 0, 0, oldWidth, oldHeight);
+            int[] mapDest = new int[newWidth * newHeight];
+            int oldx, oldy;
+            for (int y = 0; y < newHeight; y++) {
+                for (int x = 0; x < newWidth; x++) {
+                    oldx = (int) (x * fact);
+                    oldy = (int) (y * fact);
+                    mapDest[y * newWidth + x] = mapSrc[ oldy * oldWidth + oldx];
+                }
+            }
+            Bitmap dest = Bitmap.createBitmap(newWidth,newHeight, Bitmap.Config.ARGB_8888);
+            dest.setPixels(mapDest, 0, newWidth, 0, 0, newWidth, newHeight);
+            return dest;
+        }
+        return bmp;
+    }
+
     public void moyenne(Bitmap bmp, int n) {
         //Creates a matrix full of ones and applicates convolution (and divides by the number of pixels used)
         int[][] mask = new int[n][n];
@@ -106,9 +143,11 @@ public class ConvolutionMenu extends AppCompatActivity {
         int height = bmp.getHeight();
         //applicates convolution with hx et hy matrix
         int[][] hx = {{-1,0,1},{-2,0,2},{-1,0,1}};
+        //int[][] hx = {{0,0,0},{-1,0,1},{0,0,0}};
         int[][] Gx = sobelConvolutionAux(bmp,hx);
 
         int[][] hy = {{-1,-2,-1},{0,0,0},{1,2,1}};
+        //int[][] hy = {{0,-1,0},{0,0,0},{0,1,0}};
         int[][] Gy = sobelConvolutionAux(bmp,hy);
 
         int[] map = new int[width*height];
@@ -283,25 +322,23 @@ public class ConvolutionMenu extends AppCompatActivity {
 
     public void findAreaColor(int[] src, int[] borders, int x, int y, int width, int height, int[] sumRGB){
         int index = y*width+x;
-        if (borders[index]==0){
-            borders[index]=1;
-            int pixel = src[index];
-            sumRGB[0] += Color.red(pixel);
-            sumRGB[1] += Color.green(pixel);
-            sumRGB[2] += Color.blue(pixel);
-            sumRGB[3] ++;
-            if (x>0){findAreaColor(src, borders, x-1, y, width, height, sumRGB);}
-            if (x<width-1){findAreaColor(src, borders, x+1, y, width, height, sumRGB);}
-            if (y>0){findAreaColor(src, borders, x, y-1, width, height, sumRGB);}
-            if (y<height-1){findAreaColor(src, borders, x, y+1, width, height, sumRGB);}
-        }
+        borders[index]=1;
+        int pixel = src[index];
+        sumRGB[0] += Color.red(pixel);
+        sumRGB[1] += Color.green(pixel);
+        sumRGB[2] += Color.blue(pixel);
+        sumRGB[3] ++;
+        if ((x>0) && (borders[index-1]==0)){findAreaColor(src, borders, x-1, y, width, height, sumRGB);}
+        if ((x<width-1)&&(borders[index+1]==0)){findAreaColor(src, borders, x+1, y, width, height, sumRGB);}
+        if ((y>0)&&(borders[index-width]==0)){findAreaColor(src, borders, x, y-1, width, height, sumRGB);}
+        if ((y<height-1)&&(borders[index+width]==0)){findAreaColor(src, borders, x, y+1, width, height, sumRGB);}
     }
 
     public void paintArea(int[] src, int[] borders, int x, int y, int width, int height, int color){
         if (borders[y*width + x]==1){
             src[y*width+x] = color;
             borders[y*width + x] = 2;
-            if (x>0){paintArea(src, borders, x-1, y, width, height, color);}
+            if ((x>0)){paintArea(src, borders, x-1, y, width, height, color);}
             if (x<width-1){paintArea(src, borders, x+1, y, width, height, color);}
             if (y>0){paintArea(src, borders, x, y-1, width, height, color);}
             if (y<height-1){paintArea(src, borders, x, y+1, width, height, color);}
@@ -309,41 +346,74 @@ public class ConvolutionMenu extends AppCompatActivity {
     }
 
     public void cartoon(Bitmap bmp, Bitmap sobel) {
-        int w = bmp.getWidth();
-        int h = bmp.getHeight();
-        int mapBorders[] = new int[w * h];
-        Arrays.fill(mapBorders, 0);
-        int mapSrc[] = new int[w * h];
-        bmp.getPixels(mapSrc, 0, w, 0, 0, w, h);
-        int mapSobel[] = new int[w * h];
-        sobel.getPixels(mapSobel, 0, w, 0, 0, w, h);
-        int pixel, red, green, blue;
-        //Fills mapBorders with -1 at borders
-        for (int i = 0; i < w * h; i++) {
-            pixel = mapSobel[i];
-            red = Color.red(pixel);
-            green = Color.green(pixel);
-            blue = Color.blue(pixel);
-            if (red + blue + green > 50) {
-                mapBorders[i] = -1;
-            }
-        }
-        int color;
-        int[] sumRGB = new int[4];
-        for (int y=0; y<h; y++){
-            for (int x=0; x<w; x++) {
-                if ((mapBorders[y * w + x] == 0)) {
-                    Arrays.fill(sumRGB, 0);
-                    findAreaColor(mapSrc, mapBorders, x, y, w, h, sumRGB);
-                    red = sumRGB[0] / sumRGB[3];
-                    green = sumRGB[1] / sumRGB[3];
-                    blue = sumRGB[2] / sumRGB[3];
-                    color = Color.rgb(red, green, blue);
-                    paintArea(mapSrc, mapBorders, x, y, w, h, color);
+        try {
+            int w = bmp.getWidth();
+
+            int h = bmp.getHeight();
+            int mapBorders[] = new int[w * h];
+            Arrays.fill(mapBorders, 0);
+            int mapSrc[] = new int[w * h];
+            bmp.getPixels(mapSrc, 0, w, 0, 0, w, h);
+            int mapSobel[] = new int[w * h];
+            sobel.getPixels(mapSobel, 0, w, 0, 0, w, h);
+            int pixel, red, green, blue;
+            //Fills mapBorders with -1 at borders
+            for (int i = 0; i < w * h; i++) {
+                pixel = mapSobel[i];
+                red = Color.red(pixel);
+                green = Color.green(pixel);
+                blue = Color.blue(pixel);
+                if (red + blue + green > 50) {
+                    mapBorders[i] = -1;
                 }
             }
-        }
+            int color;
+            int[] sumRGB = new int[4];
+            float[] pixelHSV = new float[3];
+            for (int y = 0; y < h; y++) {
+                for (int x = 0; x < w; x++) {
+                    if (mapBorders[y * w + x] == 0) {
+                        Arrays.fill(sumRGB, 0);
+                        findAreaColor(mapSrc, mapBorders, x, y, w, h, sumRGB);
+                        red = sumRGB[0] / sumRGB[3];
+                        green = sumRGB[1] / sumRGB[3];
+                        blue = sumRGB[2] / sumRGB[3];
+                        color = Color.rgb(red, green, blue);
+                        paintArea(mapSrc, mapBorders, x, y, w, h, color);
+                    }
+                }
+            }
+            for (int i = 0; i < w * h; i++) {
+                if (mapBorders[i] == -1) {
+                    Color.colorToHSV(mapSobel[i], pixelHSV);
+                    if (pixelHSV[2] > 0.5) {
+                        int v = (int) (100 * (1 - pixelHSV[2]));
+                        mapSrc[i] = Color.rgb(v, v, v);
+                    }
+                }
+            }
             bmp.setPixels(mapSrc, 0, w, 0, 0, w, h);
+        } catch (StackOverflowError e) {
+            AlertDialog.Builder reducedialog = new AlertDialog.Builder(ConvolutionMenu.this);
+            reducedialog.setTitle("Échec")
+                    .setMessage("Il se peut que la qualité de l'image soit trop élevée. Voulez-vous réduire la qualité?")
+                    .setNegativeButton("Annuler", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    })
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            picture = reduce(picture);
+                            imgView.setImageBitmap(picture);
+                        }
+                    });
+
+            AlertDialog alert = reducedialog.create();
+            alert.show();
+        }
     }
 
     private View.OnClickListener blistener = new View.OnClickListener() {
@@ -358,9 +428,21 @@ public class ConvolutionMenu extends AppCompatActivity {
 
                 //Applicates 'moyenne' filter
                 case R.id.moy:
-                    length = Integer.valueOf(facteurLongueur.getText().toString());
-                    moyenne(picture, length);
-                    imgView.setImageBitmap(picture);
+                    try {
+                        length = Integer.valueOf(lengthText.getText().toString());
+
+                        if (length % 2 == 0) {
+                            Toast evennotodd = Toast.makeText(getApplicationContext(), "Le paramètre du filtre moyenneur doit être impair", Toast.LENGTH_LONG);
+                            evennotodd.show();
+                        } else {
+                            moyenne(picture, length);
+                            imgView.setImageBitmap(picture);
+                        }
+                    }
+                    catch(Exception e){
+                        Toast novalue = Toast.makeText(getApplicationContext(), "Aucune valeur saisie", Toast.LENGTH_LONG);
+                        novalue.show();
+                    }
                     break;
 
                 //Applicates Gaussian blur
